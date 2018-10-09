66bfa0111d6b053f2d490730fa57c9d5887ec35b
cstultz87 <cstultz@oswego.edu>
2018-10-09 17:33:10 -0500

    tbh

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
-document.querySelector('floorpicker').classList.toggle('active');
+var x = document.getElementById ('btnApt1')
+x.addEventListener ('click', showApt)
+
+function showApt() {
+document.getElementById ('aptOne').classList.toggle('active');
+}
 
