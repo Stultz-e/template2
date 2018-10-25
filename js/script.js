648b4e955f8a949e73f8f9ec05e9efe1152d22f4
cstultz87 <cstultz@oswego.edu>
2018-10-25 17:13:08 -0500

    color  template

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@
         document.querySelector('.side').classList.remove('active');
     }  
 
-    var back = document.querySelector('.btn--modal')
+    var back = document.querySelector('.btn--modal-left')
     back.addEventListener ('click', retrieveFplate) 
     
     function retrieveFplate() {
@@ -92,3 +92,48 @@ for (var i = 0; i < btns.length; i++) {
  }); */ // end
 
 
+
+
+ // MODAL SLIDER 
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
