1fd686ad53eb486c481ba832ef4e06107b293c54
cstultz87 <cstultz@oswego.edu>
2018-10-26 17:28:16 -0500

    bloop

--- js/script.js
+++ js/script.js
@@ -43,7 +43,6 @@
 
 
 
-
 // some kinda if statement that highlights the things when the thing is open 
 
 // Get the container element
@@ -88,52 +87,5 @@ for (var i = 0; i < btns.length; i++) {
     $('.image-area__exit').on('click', function() {
         $(this).closest('item').toggleClass('current'); });
 
-
  }); */ // end
 
-
-
-
- // MODAL SLIDER 
-
-
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
